f2344b46b4e947d53d52dad3796e58e038477565
dnanseldev <dnansel.dev@gmail.com>
2024-05-21 21:36:37 -0300

    Testing animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,48 @@
-import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import {
+
+  Component,
+
+  OnInit,
+
+} from '@angular/core';
+
+const enterTransition = transition(':enter', [
+  style({
+    opacity: 0,
+  }),
+  animate('1s ease-in', style({ opacity: 1 })),
+]);
+const fadeIn = trigger('fadeIn', [enterTransition]);
+
+const showMenu = trigger('showMenu', [
+  transition(':enter', [
+    style({ opacity: 0, transform: 'scale(0.95)' }),
+    animate(
+      '150ms cubic-bezier(0, 0, 0.2, 1)',
+      style({ opacity: 1, transform: 'scale(1)' })
+    ),
+  ]),
+  transition(':leave', [
+    animate(
+      '100ms cubic-bezier(0.4, 0, 1, 1)',
+      style({ opacity: 0, transform: 'scale(0.95)' })
+    ),
+  ]),
+]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [fadeIn],
 })
-export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+export class AppComponent implements OnInit {
+  show_content = false;
 
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
+  ngOnInit(): void { }
 
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
+  toggleContent() {
+    this.show_content = !this.show_content;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
